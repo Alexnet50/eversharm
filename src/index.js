1e12794de84edb830a3d260e03bf9ad9e2e564b1
Shaposhnikov Oleksiy <aladdin205@ukr.net>
2022-06-15 19:10:59 +0300

    favico, static

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 3000
 
-// import express from 'express';
-
-// const app = express()
-
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 3000;
-// }
-// app.listen(port);
-// const express = require("express");
-// require("dotenv").config();
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.listen(port, () => {
-//   console.log(`Server is running on the port ${port}.`);
-// });
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('src', path.join(__dirname, 'crc'))
+// //   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
