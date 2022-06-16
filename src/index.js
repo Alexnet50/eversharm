fc422b686127e06b637c7faf1f2f76afca211c1d
Shaposhnikov Oleksiy <aladdin205@ukr.net>
2022-06-16 11:57:58 +0300

    clean

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import express from 'express';
-import path from 'path';
-// const express = require('express')
-// const path = require('path')
-const PORT = process.env.PORT || 3000
+// import express from 'express';
+// import path from 'path';
+// // const express = require('express')
+// // const path = require('path')
+// const PORT = process.env.PORT || 3000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('src', path.join(__dirname, 'crc'))
-//   .set('view engine', 'ejs')
-  .get('/*', (req, res) => res.render('pages'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('src', path.join(__dirname, 'crc'))
+// //   .set('view engine', 'ejs')
+//   .get('/*', (req, res) => res.render('pages'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
